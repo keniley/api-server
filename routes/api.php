0f0a8e155c64a307167c5fb722cb2af25a3d770f
jelinekp <jelinekp@oxyshop.cz>
2018-03-28 12:10:58 +0200

    Add resource not found

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user','Api\UserController@list');
-Route::get('/user/{user}','Api\UserController@show');
-Route::post('/user','Api\UserController@create');
+Route::get('/user','UserController@list');
+Route::get('/user/{user}','UserController@show');
+Route::post('/user','UserController@create');
+
+// no resource
+Route::any('{path}', function ($path = null) {
+    return response()->json(['msg' => 'Resource not found', 'code' => '404'], 404);
+})->where('path', '(.*)');
